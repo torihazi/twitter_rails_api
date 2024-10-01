4e3f1dfeb694d00ef734da8dba25eaa3889f3865
torihazi <lovebarusa19981225@icloud.com>
2024-10-01 13:51:27 +0900

    set secrets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,6 +67,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 8001 }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-
-  config.require_master_key = true
 end
