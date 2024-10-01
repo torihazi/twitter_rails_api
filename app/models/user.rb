1d2e32c7e5f9d6b4b222f2e0eb415fd72cb5b4e3
torihazi <lovebarusa19981225@icloud.com>
2024-10-01 13:27:01 +0900

    pass rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable,
          :jwt_authenticatable, jwt_revocation_strategy: self
-
-  def jwt_payload
-    super
-  end
 end
